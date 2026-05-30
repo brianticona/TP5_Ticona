5998c7cb86fb454a7df0faedb0e1126a4d5e4a10
brianticona <bri.ticonam@gmail.com>
2026-05-29 23:58:27 -0300

    re ej 10

--- TP5 eje. 10/src/tp5/eje/pkg10/TP5Eje10.java
+++ TP5 eje. 10/src/tp5/eje/pkg10/TP5Eje10.java
@@ -2,78 +2,64 @@ package tp5.eje.pkg10;
 import java.util.Random;
 import java.util.Scanner;
 public class TP5Eje10 {
-    public static void main(String[] args) {
-        int[][] temp = new int[5][7];
-        lm(temp);
-        mm(temp);
-        ts(temp);
-        ps(temp);
-        tm(temp);
-    }
-    public static void lm(int[][] temp) {
-        Random random = new Random();
-        int dia=1;
-        for (int i=0;i<5;i++) {
-            for (int j=0;j<7;j++) {
-                if (dia<=31) {
-                    temp[i][j] = random.nextInt(32) + 7;
+    static int[][] m = new int[5][7];
+    public static void lm() {
+        Random r = new Random();
+        int dia = 1;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (dia <= 31) {
+                    m[i][j] = r.nextInt(32) + 7;
                     dia++;
+                } else {
+                    m[i][j] = 0;
                 }
             }
         }
     }
-    public static void mm(int[][] temp) {
-        System.out.println("Temperaturas:\n");
-        for (int i=0;i<5;i++) {
-            for (int j=0;j<7;j++) {
-                System.out.print("[" + temp[i][j] + "]");
+    public static void ts(int semana) {
+        String[] dias = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+        int max = m[semana][0];
+        int min = m[semana][0];
+        int diaMax = 0;
+        int diaMin = 0;
+        for (int i = 0; i < 7; i++) {
+            if (m[semana][i] > max) {
+                max = m[semana][i];
+                diaMax = i;
+            }
+            if (m[semana][i] < min && m[semana][i] != 0) {
+                min = m[semana][i];
+                diaMin = i;
+            }
+        }
+        System.out.println("Temperatura mas alta fue: " + max + "°C - " + dias[diaMax]);
+        System.out.println("Temperatura mas baja fue: " + min + "°C - " + dias[diaMin]);
+    }
+    public static double ps(int semana) {
+        int suma = 0;
+        int cantidad = 0;
+        for (int i = 0; i < 7; i++) {
+            if (matriz[semana][i] != 0) {
+                suma += matriz[semana][i];
+                cantidad++;
+            }
+        }
+        return (double) suma / cantidad;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        lm();
+        System.out.println("Temperaturas del mes:");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(matriz[i][j] + "\t");
             }
             System.out.println();
         }
-    }
-    public static void ts (int[][] temp) {
-        for (int i=0;i<5;i++) {
-            int mayor=temp[i][0];
-            int menor=temp[i][0];
-            int dm=0;
-            int dme=0;
-            for (int j=0;j<7;j++) {
-                if (temp[i][j]>mayor) {
-                    mayor=temp[i][j];
-                    dm=j;
-                }
-                if (temp[i][j]<menor) {
-                    menor=temp[i][j];
-                    dme=j;
-                }
-            }
-            System.out.println("Semana " + (i + 1) + " | Mayor: " + mayor + " dia " + dme + " | Menor: " + menor + " dia " + dme);
-        }
-    }
-    public static void ps(int[][] temp) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese semana (1-5): ");
-        int s = scanner.nextInt() - 1;
-        int suma = 0;
-        for (int j=0;j<7;j++) {
-            suma +=temp[s][j];
-        }
-        double p=(double)suma/7;
-        System.out.println("Promedio semana " + (s + 1) + ": " + p);
-    }
-    public static void tm (int[][] temp) {
-        int mayor=temp[0][0];
-        int sm=0;
-        int dm=0;
-        for (int i=0;i<5;i++) {
-            for (int j=0;j<7;j++) {
-                if (temp[i][j]>mayor) {
-                    mayor = temp[i][j];
-                    sm=i;
-                    dm=j;
-                }
-            }
-        }
-        System.out.println("Mayor temperatura del mes es " + mayor + " de la semana " + (sm + 1) + "  del dia " + dm+ ".");
+        System.out.print("\nIngrese una semana (1 a 5): ");
+        int semana = sc.nextInt() - 1;
+        ts(semana);
+        System.out.println("Promedio: " + ps(semana) + "°C");
     }
 }
